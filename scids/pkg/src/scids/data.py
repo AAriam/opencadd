812a9134ef29e396dd281bf7762c2fc4931409fc
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-02 05:24:53 +0200

    move consts to scids

--- scids/pkg/src/scids/data.py
+++ scids/pkg/src/scids/data.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import Enum
 from typing import Literal
 
@@ -65,3 +67,30 @@ class Autodock4AtomType(Enum):
         H-bonding atoms, and 0 for non H-bonding atoms.
         """
         return self.value[2]
+
+
+
+def _van_der_waals_radius(
+    atom_ids: ArrayLike, id_type: Literal["internal", "element_symbol"] = "internal"
+) -> np.ndarray:
+    """
+    Get the Van der Waals radii of a set of given atoms.
+
+    Parameters
+    ----------
+    atom_ids : opencadd.typing.ArrayLike
+        1D array of atom identifiers.
+    id_type : Literal["internal", "element_symbol"], optional, default: "internal"
+        Type of atom identifiers.
+
+    Returns
+    -------
+    numpy.ndarray
+        1D array of van der Waals radii corresponding to the input array.
+
+    References
+    ----------
+    https://github.com/rdkit/rdkit/issues/1831
+
+    """
+    raise NotImplementedError
