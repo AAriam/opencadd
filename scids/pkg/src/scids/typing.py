db42cac38c8af61738151ff2a89f6ab4ac4fb138
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-03 02:14:59 +0200

    Update typing.py

--- scids/pkg/src/scids/typing.py
+++ scids/pkg/src/scids/typing.py
@@ -1,5 +1,10 @@
-from typing import IO, TypeAlias
+from typing import IO, TypeAlias, Sequence
 from pathlib import Path
+
+import jax.numpy as jnp
+import numpy as np
+
 
 PathLike: TypeAlias = str | Path
 FileContentLike: TypeAlias = str | bytes | IO
+ArrayLike: TypeAlias = Sequence | np.ndarray | jnp.ndarray
