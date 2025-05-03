b71584b6bb6c64c99b4e9a3292d17b28a61ea75e
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-03 09:58:13 +0200

    Update __init__.py

--- scids/pkg/src/scids/file/__init__.py
+++ scids/pkg/src/scids/file/__init__.py
@@ -1,8 +1,10 @@
 """Read and write data files."""
 
-from scids.file import autodock_gpf, autodock_map
+from scids.file import autodock_gpf, autodock_map, mrc, pdb
 
 __all__ = [
     "autodock_gpf",
     "autodock_map",
+    "mrc",
+    "pdb",
 ]
