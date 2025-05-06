b38e26ece2df46cfbfcec493d6cc0cdb7b571296
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-06 15:13:33 +0200

    Update mrc.py

--- scifile/pkg/src/scifile/mrc.py
+++ scifile/pkg/src/scifile/mrc.py
@@ -93,7 +93,7 @@ class MrcFile:
         i.e., columns, rows, and sections (fastest to slowest axis).
     m_xyz
         Number of intervals (samples - 1)
-        along each axis in the full unit cell.
+        along each axis in the unit cell.
         This corresponds to [MX, MY, MZ],
         i.e., columns, rows, and sections (fastest to slowest axis).
     cell_a
@@ -146,11 +146,12 @@ class MrcFile:
         if len(self.labels) > 10:
             raise ValueError("Maximum of 10 labels allowed")
         # ensure origin is ndarray
-        self.origin = np.array(self.origin, dtype=np.float32)
+        self.nstart_xyz = np.asarray(self.nstart_xyz)
+        self.origin = np.asarray(self.origin)
         if self.origin.shape != (3,):
             raise ValueError("origin must be a 3-element float32 array")
-        self.cell_a = np.array(self.cell_a, dtype=np.float32)
-        self.cell_b = np.array(self.cell_b, dtype=np.float32)
+        self.cell_a = np.asarray(self.cell_a)
+        self.cell_b = np.asarray(self.cell_b)
         if self.cell_a.shape != (3,) or self.cell_b.shape != (3,):
             raise ValueError("cell_a and cell_b must be 3-element float32 vectors")
         if len(self.extra) != 8:
@@ -224,6 +225,65 @@ class MrcFile:
         """Number of labels in the header."""
         return len(self.labels)
 
+    @property
+    def origin_recommended(self) -> np.ndarray:
+        """Real-space coordinates (in Ångströms) of the origin voxel (0, 0, 0).
+
+        This returns the explicitly defined `origin` if `nversion > 0`,
+        otherwise returns `grid_origin`.
+        """
+        return self.origin if self.nversion > 0 else self.grid_origin
+
+    @property
+    def cell_vectors(self) -> np.ndarray:
+        """Unit cell vectors in Ångströms.
+
+        This is a 3x3 array where each row (i.e. cell_vectors[i])
+        is the i-th unit cell vector.
+        For example, for an orthogonal grid, this is a diagonal matrix
+        where the diagonal elements are the `cell_a` values.
+        """
+        # Calculate unit cell vectors based on cell_a and cell_b
+        a, b, c = self.cell_a
+        alpha, beta, gamma = np.radians(self.cell_b)
+        # Based on crystallography conventions
+        va = np.array([a, 0.0, 0.0])
+        vb = np.array([b * np.cos(gamma), b * np.sin(gamma), 0.0])
+        cx = c * np.cos(beta)
+        cy = c * (np.cos(alpha) - np.cos(beta) * np.cos(gamma)) / np.sin(gamma)
+        cz = np.sqrt(c**2 - cx**2 - cy**2)
+        vc = np.array([cx, cy, cz])
+        return np.stack([va, vb, vc], axis=0)
+
+    @property
+    def grid_vectors(self) -> np.ndarray:
+        """Grid basis vectors (a.k.a. grid spacing matrix) in Ångströms.
+
+        This is a 3x3 array where each row (i.e. grid_vectors[i])
+        is a vector from one point to the next point in the i-th dimension.
+        For example, for an orthogonal grid, this is a diagonal matrix
+        where the diagonal elements are the grid spacing in each dimension.
+        """
+        return self.cell_vectors / self.m_xyz.reshape(3, 1)
+
+    @property
+    def grid_origin(self) -> np.ndarray:
+        """Origin of the grid in Ångströms.
+
+        This is a 3-element array where each element is the
+        coordinate of the origin in the corresponding dimension.
+        """
+        return self.nstart_xyz @ self.grid_vectors
+
+    @property
+    def grid_center(self) -> np.ndarray:
+        """Center of the grid in Ångströms.
+
+        This is a 3-element array where each element is the
+        coordinate of the center in the corresponding dimension.
+        """
+        return self.grid_origin + self.grid_vectors @ ((self.n_xyz - 1) / 2)
+
     def __bytes__(self) -> bytes:
         """Serialize the MRC/CCP4 file to bytes."""
         # Write header
@@ -283,8 +343,27 @@ class MrcFile:
             raise ValueError(f"Unsupported MODE {self.mode} during serialization")
         return header.tobytes() + self.extended_header + data_bytes
 
+    def __repr__(self) -> str:
+        def fmt(name, value):
+            if isinstance(value, np.ndarray):
+                val_str = np.array2string(value, separator=" ", max_line_width=80)
+            elif isinstance(value, bytes):
+                val_str = repr(value.strip(b"\x00"))
+            elif isinstance(value, str):
+                val_str = f'"{value}"'
+            elif isinstance(value, list):
+                val_str = "[" + ", ".join(f'"{v}"' for v in value) + "]"
+            else:
+                val_str = str(value)
+            return f"   {name:<{arg_name_max_len}} = {val_str}"
+        arg_names = list(name for name in HEADER_DTYPE.fields.keys() if name not in ("map", "machst", "label"))
+        arg_name_max_len = max(len(name) for name in arg_names)
+        arg_names.append("labels")
+        args = "\n".join(fmt(name, getattr(self, name)) for name in arg_names)
+        return f"{self.__class__.__name__}(\n{args}\n)"
 
-def parse(file: bytes | Path) -> MrcFile:
+
+def read(file: bytes | Path) -> MrcFile:
     """Read an MRC/CCP4 file.
 
     Parameters
@@ -385,12 +464,9 @@ def parse(file: bytes | Path) -> MrcFile:
         exttyp=header["exttyp"].tobytes().decode("ascii", errors="replace").rstrip("\x00"),
         nversion=header["nversion"],
         extra2=bytes(header["extra2"]),
-        origin=(
-            header["origin"] if header["nversion"] > 0
-            else header["nstart_xyz"] * header["cell_a"] / header["m_xyz"]
-        ),
+        origin=header["origin"],
         labels=[
-            label.rstrip(b"\x00").decode("ascii", errors="replace")
+            label.rstrip(b"\x00").decode("ascii", errors="replace").strip()
             for label in header["label"][:header["nlabl"]]
         ],
         extended_header=(
