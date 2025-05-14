fd959188130bae3ae0b6bc7df471be13a73e1e18
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-14 18:45:46 +0200

    Update __init__.py

--- scids/pkg/src/scids/__init__.py
+++ scids/pkg/src/scids/__init__.py
@@ -1,5 +1,9 @@
 """SciDS: Scientific Data Structures and file formats"""
 
+import jax
+# Enable 64-bit floating point precision for JAX
+# See: https://docs.jax.dev/en/latest/notebooks/Common_Gotchas_in_JAX.html#double-64bit-precision
+jax.config.update("jax_enable_x64", True)
 
 from scids import field, grid, pointcloud, volume
 
