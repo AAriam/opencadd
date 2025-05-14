390a1fe9edbfb6f9ef9a585aefd008904d2a27c9
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-14 02:39:13 +0200

    Update __init__.py

--- scids/pkg/src/scids/__init__.py
+++ scids/pkg/src/scids/__init__.py
@@ -1,13 +1,12 @@
 """SciDS: Scientific Data Structures and file formats"""
 
 
-from scids import field, grid, pointcloud, rectangular_cuboid, volume
+from scids import field, grid, pointcloud, volume
 
 
 __all__ = [
     "field",
     "grid",
     "pointcloud",
-    "rectangular_cuboid",
     "volume",
 ]
