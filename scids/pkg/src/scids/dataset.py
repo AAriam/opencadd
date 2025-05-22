af562f31306ca1115d4063950fbfeaa62b9f6fe9
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-05-23 00:14:55 +0200

    Update dataset.py

--- scids/pkg/src/scids/dataset.py
+++ scids/pkg/src/scids/dataset.py
@@ -16,99 +16,99 @@ class DataSet:
     def __init__(
         self,
         data: ArrayLike,
-        prefix: int | jnp.integer | Sequence[str | tuple[str, Sequence[str]]],
+        batch: int | jnp.integer | Sequence[str | tuple[str, Sequence[str]]],
     ):
         self._data = jnp.asarray(data)
-        prefix_is_int = isinstance(prefix, int | jnp.integer)
-        self._prefix_ndim = prefix if prefix_is_int else len(prefix)
-        if self._data.ndim <= self._prefix_ndim:
+        batch_is_int = isinstance(batch, int | jnp.integer)
+        self._batch_ndim = batch if batch_is_int else len(batch)
+        if self._data.ndim <= self._batch_ndim:
             raise exception.InputError(
-                name="prefix",
-                message="The prefix dimension must be less than the data dimension, "
-                        f"but got {self._prefix_ndim} and {self._data.ndim}."
+                name="batch",
+                message="The batch dimension must be smaller than the data dimension, "
+                        f"but got {self._batch_ndim}D batch for {self._data.ndim}D data."
             )
-        self._prefix_shape = self._data.shape[:self._prefix_ndim]
-        self._prefix_size = np.prod(self._prefix_shape)
-        self._prefix_dim_labels = []
-        self._prefix_instance_labels = {}
-        if prefix_is_int:
+        self._batch_shape = self._data.shape[:self._batch_ndim]
+        self._batch_size = np.prod(self._batch_shape)
+        self._batch_dim_labels = []
+        self._batch_instance_labels = {}
+        if batch_is_int:
             return
-        for prefix_idx, prefix_data in enumerate(prefix):
-            if isinstance(prefix_data, str):
-                self._prefix_dim_labels.append(prefix_data)
+        for batch_idx, batch_data in enumerate(batch):
+            if isinstance(batch_data, str):
+                self._batch_dim_labels.append(batch_data)
                 continue
-            prefix_dim_label, prefix_instance_labels = prefix_data
-            self._prefix_dim_labels.append(prefix_dim_label)
-            if len(prefix_instance_labels) != self._prefix_shape[prefix_idx]:
+            batch_dim_label, batch_instance_labels = batch_data
+            self._batch_dim_labels.append(batch_dim_label)
+            if len(batch_instance_labels) != self._batch_shape[batch_idx]:
                 raise exception.InputError(
-                    name="prefix",
-                    message="The number of prefix instances must match the shape of the tensor along the prefix dimension, "
-                            f"but got {len(prefix_instance_labels)} instances for prefix dimension {prefix_idx} with size {self._prefix_shape[prefix_idx]}."
+                    name="batch",
+                    message="The number of specified batch instances must match the shape of the data along that batch dimension, "
+                            f"but got {len(batch_instance_labels)} instances for batch axis {batch_idx} with size {self._batch_shape[batch_idx]}."
                 )
-            self._prefix_instance_labels[prefix_dim_label] = np.array(prefix_instance_labels)
-        self._prefix_dim_labels = np.array(self._prefix_dim_labels)
+            self._batch_instance_labels[batch_dim_label] = np.array(batch_instance_labels)
+        self._batch_dim_labels = np.array(self._batch_dim_labels)
         return
 
     @property
-    def prefix_ndim(self) -> int:
-        """Number of prefix dimensions."""
-        return self._prefix_ndim
+    def batch_ndim(self) -> int:
+        """Number of batch dimensions."""
+        return self._batch_ndim
 
     @property
-    def prefix_shape(self) -> np.ndarray:
-        """Shape of the prefix dimensions."""
-        return np.array(self._prefix_shape)
+    def batch_shape(self) -> np.ndarray:
+        """Shape of the batch dimensions."""
+        return np.array(self._batch_shape)
 
     @property
-    def prefix_size(self) -> int:
-        """Size of the prefix dimensions.
+    def batch_size(self) -> int:
+        """Size of the batch dimensions.
 
         This represents the total number of instances.
         """
-        return self._prefix_size
+        return self._batch_size
 
     @property
-    def prefix_labels(self) -> np.ndarray:
+    def batch_labels(self) -> np.ndarray:
         """Labels of the prefix dimensions."""
-        return np.array(self._prefix_dim_labels)
+        return np.array(self._batch_dim_labels)
 
     @property
-    def prefix_instance_labels(self) -> dict[str, np.ndarray]:
-        """Labels of the prefix dimensions' instances."""
-        return {k: np.array(v) for k, v in self._prefix_instance_labels.items()}
+    def batch_instance_labels(self) -> dict[str, np.ndarray]:
+        """Labels of the batch dimensions' instances."""
+        return {k: np.array(v) for k, v in self._batch_instance_labels.items()}
 
-    def prefix_index(self, labels: str | Sequence[str]) -> np.ndarray:
-        """Get the indices of prefix dimensions from their labels."""
+    def batch_index(self, labels: str | Sequence[str]) -> np.ndarray:
+        """Get the indices of batch dimensions from their labels."""
         names = np.asarray(labels).reshape(-1, 1)
-        indices = np.argwhere(self.prefix_labels == names)
+        indices = np.argwhere(self.batch_labels == names)
         if indices.shape[0] != names.size:
             ind_bad_names = np.setdiff1d(np.arange(names.size), indices[:, 0])
             raise IndexError(
-                f"Following prefix labels are not valid: {names[ind_bad_names]}. "
-                f"Valid labels are: {self.prefix_labels}."
+                f"Following batch labels are not valid: {names[ind_bad_names]}. "
+                f"Valid labels are: {self.batch_labels}."
             )
         return np.squeeze(indices[:, 1])
 
     def __call__(self, **kwargs) -> jnp.ndarray:
-        if not self._prefix_instance_labels:
+        if not self._batch_instance_labels:
             raise exception.InputError(
-                name="prefix",
-                message="Prefix dimension labels are not set. "
-                        "Please provide a prefix dimension label to index the tensor."
+                name="batch",
+                message="Batch dimension labels are not set. "
+                        "Please provide a batch dimension label to index the data."
             )
         index = []
-        for prefix_label in self._prefix_dim_labels:
-            if prefix_label not in kwargs:
+        for batch_label in self._batch_dim_labels:
+            if batch_label not in kwargs:
                 index.append(slice(None))
                 continue
-            instance_labels = self._prefix_instance_labels.get(prefix_label, [])
-            selection = kwargs[prefix_label]
+            instance_labels = self._batch_instance_labels.get(batch_label, [])
+            selection = kwargs[batch_label]
             if isinstance(selection, str):
                 selection_idx = np.argwhere(instance_labels == selection)
                 if selection_idx.size == 0:
                     raise exception.InputError(
-                        name="prefix",
-                        message=f"Prefix instance label '{selection}' is not valid for prefix dimension '{prefix_label}'. "
+                        name="batch",
+                        message=f"Batch instance label '{selection}' is not valid for batch dimension '{batch_label}'. "
                                 f"Valid labels are: {instance_labels}."
                     )
                 index.append(selection_idx[0][0])
