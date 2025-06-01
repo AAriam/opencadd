c206a47beabace4c50ae39f1eb3e99b193b52e87
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-01 20:40:49 +0200

    Update __init__.py

--- caddpy/pkg/src/caddpy/__init__.py
+++ caddpy/pkg/src/caddpy/__init__.py
@@ -1,8 +1,9 @@
 """CADDPy: Computer-Aided Drug Design in Python"""
 
-from caddpy import chemsys, mif
+from caddpy import chemsys, mif, pocket
 
 __all__ = [
     "chemsys",
     "mif",
+    "pocket",
 ]
