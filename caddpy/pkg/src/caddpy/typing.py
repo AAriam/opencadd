f984fa1afe5b3eac660a064baa7094d7f1dedd5e
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-06 15:03:46 +0200

    Update typing.py

--- caddpy/pkg/src/caddpy/typing.py
+++ caddpy/pkg/src/caddpy/typing.py
@@ -1,15 +1,23 @@
+from functools import partial
 from collections.abc import Sequence
 from pathlib import Path
 from typing import IO, TypeAlias
 
+import jax
 import jax.numpy as jnp
 import numpy as np
 
+from beartype import beartype
+from beartype.vale import Is
+from jaxtyping import jaxtyped, Num, Bool, Float, Shaped, Int
 
+typecheck = beartype
+atypecheck = partial(jaxtyped, typechecker=beartype)
+
+Array: TypeAlias = jax.Array | np.ndarray
+JAXArray: TypeAlias = jax.Array
 
 PathLike: TypeAlias = str | Path
 FileContentLike: TypeAlias = str | bytes | IO
 FileLike: TypeAlias = PathLike | FileContentLike
 ArrayLike: TypeAlias = Sequence | np.ndarray | jnp.ndarray
-
-
