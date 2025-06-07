226e498d2af4defe55b72303741119628aa31391
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-07 23:21:41 +0200

    Update __init__.py

--- scishow/pkg/src/scishow/__init__.py
+++ scishow/pkg/src/scishow/__init__.py
@@ -1,8 +1,9 @@
 """SciShow: Scientific data visualization."""
 
-from scishow import matplotlib, nglview
+from scishow import matplotlib, nglview, widgets
 
 __all__ = [
     "matplotlib",
     "nglview",
+    "widgets",
 ]
