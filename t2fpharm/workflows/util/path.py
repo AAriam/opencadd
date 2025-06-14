4c47666df7e545d9ee25cfe9ac9b4b3a63e5c78d
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-14 20:26:42 +0200

    Create path.py

---
+++ t2fpharm/workflows/util/path.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+_DATA_DIR = Path(__file__).parent.parent.parent / "data"
+INPUT = _DATA_DIR / "input"
+INTERMEDIATE = _DATA_DIR / "intermediate"
+OUTPUT = _DATA_DIR / "output"
