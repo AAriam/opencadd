a5ce2a3d69d91bd2840cfcf470e97e9f32bb5343
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-20 16:56:08 +0200

    Update __init__.py

--- t2fpharm/pkg/src/t2fpharm/__init__.py
+++ t2fpharm/pkg/src/t2fpharm/__init__.py
@@ -1,6 +1,9 @@
-from t2fpharm import input, original
 
-__all__ = [
-    "input",
-    "original",
-]
+from t2fpharm.simple import T2FPharm
+
+# from t2fpharm import input, original
+
+# __all__ = [
+#     "input",
+#     "original",
+# ]
