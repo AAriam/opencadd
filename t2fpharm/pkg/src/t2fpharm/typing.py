2dcbd9217be5ad8f79bc32fd855b6b80cc13c5f3
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-27 21:56:09 +0200

    Create typing.py

---
+++ t2fpharm/pkg/src/t2fpharm/typing.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+import jax.numpy as jnp
+from pydantic import Field
+
+
+PositiveInt = Annotated[
+    int,
+    Field(gt=0, description="A positive integer value."),
+]
+PositiveFloat = Annotated[
+    float,
+    Field(gt=0, description="A positive float value."),
+]
+
+PositiveIntTuple = Annotated[
+    tuple[PositiveInt, ...],
+    Field(
+        min_length=1,
+        description="A tuple of positive integer values."
+    )
+]
+PositiveFloatTuple = Annotated[
+    tuple[PositiveFloat, ...],
+    Field(
+        min_length=1,
+        description="A tuple of positive float values."
+    )
+]
+
+
+def is_real_number(value) -> bool:
+    """Check if the value is a real number (int or float).
+
+    This covers both native Python types, as well as JAX/NumPy types.
+    """
+    return is_integer(value) or is_float(value)
+
+
+def is_integer(value) -> bool:
+    """Check if the value is an integer (int or np.integer).
+
+    This covers both native Python types, as well as JAX/NumPy types.
+    """
+    return jnp.issubdtype(type(value), jnp.integer)
+
+
+def is_float(value) -> bool:
+    """Check if the value is a float (float or np.floating).
+
+    This covers both native Python types, as well as JAX/NumPy types.
+    """
+    return jnp.issubdtype(type(value), jnp.floating)
