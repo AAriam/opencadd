f484facc538f58b10e74d70f0727e19ae205ca79
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-06-30 13:43:17 +0200

    Update pocket.py

--- caddpy/pkg/src/caddpy/pocket/pocket.py
+++ caddpy/pkg/src/caddpy/pocket/pocket.py
@@ -2,10 +2,14 @@
 from typing import Sequence
 
 import jax.numpy as jnp
-from caddpy.typing import ArrayLike
+
+import arrayer
+import scids
 from scids.field import Field
 from scids.grid import Grid
+import scishow
 
+from caddpy.typing import ArrayLike
 
 class Pocket(Field):
     """Binding pocket."""
@@ -32,5 +36,56 @@ class Pocket(Field):
                     "than the number of dimensions of the tensor, "
                     f"but got {batch_ndim} batch dimensions for a {tensor.ndim}D tensor."
                 )
+        tensor, deltas = arrayer.tensor.ensure_padding(
+            tensor=tensor,
+            axes=tuple(range(batch_ndim, tensor.ndim)),
+            padding=0,
+            pad_value=False,
+        )
+        origin_shift = jnp.array([delta[0] for delta in deltas]) * grid.spacings
+        new_origin = grid.lower_bounds + origin_shift
+        grid = scids.grid.from_anchor_shape_spacing(
+            shape=tensor.shape[batch_ndim:],
+            spacing=grid.spacings,
+            anchor_type="lower",
+            anchor_coord=new_origin,
+        )
         super().__init__(tensor=tensor, grid=grid, batch=batch)
+        tensor_dialated, deltas = arrayer.tensor.ensure_padding(
+            tensor=tensor,
+            axes=tuple(range(batch_ndim, tensor.ndim)),
+            padding=3,
+            pad_value=False,
+        )
+        self._tensor_dialated = tensor_dialated.astype(jnp.uint8)
+        self._grid_dialated = scids.grid.from_anchor_shape_spacing(
+            shape=self._tensor_dialated.shape[batch_ndim:],
+            spacing=grid.spacings,
+            anchor_type="lower",
+            anchor_coord=grid.lower_bounds - 3 * grid.spacings,
+        )
         return
+
+    def display(
+        self,
+        nglwidget=None,
+        name: str = "Pocket",
+        contour: bool = True,
+        visible: bool = True,
+        color: tuple[float, float, float] = (0.8, 0.2, 0.2)
+    ):
+        nv = nglwidget or scishow.nglview.NGLWidget()
+        nv.add_volume(
+            data=self._tensor_dialated,
+            basis=self._grid_dialated.unit_vectors,
+            origin=self._grid_dialated.lower_bounds,
+            name=name,
+            representation_params=scishow.nglview.SurfaceRepresentationParameters(
+                isolevel=0.5,
+                isolevel_type="value",
+                contour=contour,
+                color=color,
+                visible=visible,
+            )
+        )
+        return nv
