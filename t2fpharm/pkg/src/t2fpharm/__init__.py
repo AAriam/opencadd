dc7f520ea93036ff621bc6a0e822876fa72f82df
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-02 23:24:07 +0200

    Update __init__.py

--- t2fpharm/pkg/src/t2fpharm/__init__.py
+++ t2fpharm/pkg/src/t2fpharm/__init__.py
@@ -1,4 +1,5 @@
-from t2fpharm import field, grid, ligand, pocket, receptor, modeler
+from t2fpharm import field, grid, ligand, pocket, receptor
+from t2fpharm.modeler import Modeler
 
 __all__ = [
     "field",
@@ -10,10 +11,17 @@ __all__ = [
 
 
 def load(
-    pocket: pocket.Pocket,
     field: field.Field,
-):
-    return modeler.Modeler(
-        pocket=pocket,
-        field=field,
-    )
+    pocket: pocket.Pocket | None = None,
+) -> Modeler:
+    """Create a target-based pharmacophore modeler.
+
+    Parameters
+    ----------
+    field
+        Field containing energy (or similar) values
+        for pharmacophore matching.
+    pocket
+        Additional pocket mask for the field.
+    """
+    return Modeler(pocket=pocket, field=field)
