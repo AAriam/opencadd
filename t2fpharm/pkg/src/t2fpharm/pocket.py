efac0fe851d7e4350260ddd6482ffd1a19883780
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-02 04:03:56 +0200

    Update pocket.py

--- t2fpharm/pkg/src/t2fpharm/pocket.py
+++ t2fpharm/pkg/src/t2fpharm/pocket.py
@@ -1,31 +1,8 @@
+from caddpy.pocket import Pocket, from_data, from_dogsite, detector
 
-
-class Pocket:
-    def __init__(
-        self,
-        origin,
-        spacing,
-        voxels,
-    ):
-        self._origin = origin
-        self._spacing = spacing
-        self._voxels = voxels
-        self._voxel_volume = self.spacing ** 3
-        return
-
-    @property
-    def origin(self):
-        return self._origin
-
-    @property
-    def spacing(self):
-        return self._spacing
-
-    @property
-    def voxels(self):
-        return self._voxels
-
-    @property
-    def voxel_volume(self):
-        """Calculate the volume of a single voxel."""
-        return self._voxel_volume
+__all__ = [
+    "Pocket",
+    "from_data",
+    "from_dogsite",
+    "detector",
+]
