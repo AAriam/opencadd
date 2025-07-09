354be183592cc12630dbc4ccffccc1835b2f988f
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-09 22:21:35 +0200

    Update pocket.py

--- t2fpharm/pkg/src/t2fpharm/pocket.py
+++ t2fpharm/pkg/src/t2fpharm/pocket.py
@@ -1,9 +1,10 @@
-from caddpy.pocket import Pocket, from_data, from_dogsite, from_ligand, detector
+from caddpy.pocket import Pocket, from_data, from_dogsite, from_ligand, from_npz, detector
 
 __all__ = [
     "Pocket",
     "from_data",
     "from_dogsite",
     "from_ligand",
+    "from_npz",
     "detector",
 ]
