e19ccc1fb6c31e920aed75db6aaed9966a8f222f
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-10 22:39:37 +0200

    Update __init__.py

--- t2fpharm/pkg/src/t2fpharm/__init__.py
+++ t2fpharm/pkg/src/t2fpharm/__init__.py
@@ -1,21 +1,33 @@
 """T2FPharm: Truly Target Focused Pharmacophore Modeler."""
 
-from t2fpharm import field, grid, ligand, pocket, receptor
+from t2fpharm import field, grid, pharm, pocket, system
+from t2fpharm.field import Field
+from t2fpharm.grid import Grid
 from t2fpharm.modeler import Modeler
+from t2fpharm.pharm import Pharmacophore
+from t2fpharm.pocket import Pocket
+from t2fpharm.system import System
+
 
 __all__ = [
     "field",
     "grid",
-    "ligand",
+    "pharm",
     "pocket",
-    "receptor",
+    "system",
+    "Field",
+    "Grid",
+    "Modeler",
+    "Pharmacophore",
+    "Pocket",
+    "System",
 ]
 
 
 def modeler(
-    field: field.Field,
-    pocket: pocket.Pocket | None = None,
-    receptor: receptor.Receptor | None = None,
+    field: Field,
+    pocket: Pocket | None = None,
+    receptor: System | None = None,
 ) -> Modeler:
     """Create a target-focused pharmacophore modeler.
 
