6a4904a33dbd3f91f9ddf2fb337249282d280015
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-10 22:40:24 +0200

    rename

--- t2fpharm/pkg/src/t2fpharm/receptor.py
+++
@@ -1,6 +0,0 @@
-from caddpy.chemsys import ChemicalSystem as Receptor, from_pdb
-
-__all__ = [
-    "Receptor",
-    "from_pdb",
-]
---
+++ t2fpharm/pkg/src/t2fpharm/system.py
@@ -0,0 +1,6 @@
+from caddpy.chemsys import ChemicalSystem as System, from_pdb
+
+__all__ = [
+    "System",
+    "from_pdb",
+]
