90d364e09710aa80c380b0502b25bb31e3fe5e7b
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-14 16:06:31 +0200

    Update __init__.py

--- t2fpharm/pkg/src/t2fpharm/input/modeler/__init__.py
+++ t2fpharm/pkg/src/t2fpharm/input/modeler/__init__.py
@@ -1,7 +1,10 @@
 from t2fpharm.input.modeler.cnn import CNNInput
 from t2fpharm.input.modeler.largest_peaks import LargestPeaksInput
+from t2fpharm.input.modeler.simple import SimpleInput
+
 
 __all__ = [
     "CNNInput",
     "LargestPeaksInput",
+    "SimpleInput"
 ]
