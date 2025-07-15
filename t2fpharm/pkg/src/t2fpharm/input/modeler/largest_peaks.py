67e8f98012d6e9762a76ac6f73a0f019b146682e
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-15 18:19:43 +0200

    Update largest_peaks.py

--- t2fpharm/pkg/src/t2fpharm/input/modeler/largest_peaks.py
+++ t2fpharm/pkg/src/t2fpharm/input/modeler/largest_peaks.py
@@ -3,11 +3,11 @@ from typing import Literal
 from pydantic import model_validator
 
 from t2fpharm.input import validator
-from t2fpharm.input.modeler.simple import SimpleInput
+from t2fpharm.input.modeler.simple import ModelerSimpleInput
 from t2fpharm.typing import PositiveInt, PositiveFloat
 
 
-class LargestPeaksInput(SimpleInput):
+class ModelerLargestPeaksInput(ModelerSimpleInput):
     """Validator for arguments of the `Modeler.largest_peaks` method.
 
     In addition to the arguments defined as fields below,
@@ -36,7 +36,7 @@ class LargestPeaksInput(SimpleInput):
     priority_factor: dict[str, float | None]
 
     @model_validator(mode="before")
-    def _preprocess(cls, values: dict[str, object]) -> dict[str, object]:
+    def _validate_modeler_largest_peak_input(cls, values: dict[str, object]) -> dict[str, object]:
         """Preprocess and validate the input values."""
         feature_types = values["feature_types"]
         for argname, fill_value, none_allowed in (
