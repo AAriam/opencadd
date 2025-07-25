86ed1b08ce72c408920c5fa53c6f67d9cbfe9625
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-25 19:21:02 +0200

    Update io.py

--- t2fpharm/study/pkg/src/t2fpharm_study/io.py
+++ t2fpharm/study/pkg/src/t2fpharm_study/io.py
@@ -11,15 +11,20 @@ def read_pharm_df(
     """Read a pharmacophore DataFrame from a file."""
     dirpath = Path(dirpath)
     filepath = dirpath / _pharm_df_filename(pdb_id, job_idx)
-    if not filepath.is_file():
-        raise FileNotFoundError(
-            f"DataFrame file not found at '{filepath}'."
-        )
-    df = pd.read_parquet(filepath, engine="pyarrow")
+    df = read_df(filepath=filepath)
     for col_name in ["label", "target_label"]:
         if col_name in df.columns and df[col_name].dtype == "object":
             df[col_name] = df[col_name].apply(tuple)
     return df
+
+
+def read_df(filepath: Path | str) -> pd.DataFrame:
+    filepath = Path(filepath).resolve()
+    if not filepath.is_file():
+        raise FileNotFoundError(
+            f"DataFrame file not found at '{filepath}'."
+        )
+    return pd.read_parquet(filepath, engine="pyarrow")
 
 
 def write_pharm_df(
@@ -29,10 +34,20 @@ def write_pharm_df(
     job_idx: int,
 ) -> None:
     """Write a pharmacophore DataFrame to a file."""
-    dirpath = Path(dirpath)
-    dirpath.mkdir(parents=True, exist_ok=True)
+    return write_df(
+        df=df,
+        filepath=Path(dirpath) / _pharm_df_filename(pdb_id, job_idx),
+    )
+
+
+def write_df(
+    df: pd.DataFrame,
+    filepath: Path | str,
+) -> None:
+    filepath = Path(filepath)
+    filepath.parent.mkdir(parents=True, exist_ok=True)
     df.to_parquet(
-        path=dirpath / _pharm_df_filename(pdb_id, job_idx),
+        path=filepath,
         engine="pyarrow",
         compression="zstd",
         compression_level=3,
