e096c9c3ddc837db45c7b87418a21006889fd0df
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-09-16 04:44:09 +0200

    Update system.py

--- t2fpharm/pkg/src/t2fpharm/system.py
+++ t2fpharm/pkg/src/t2fpharm/system.py
@@ -1,7 +1,8 @@
-from caddpy.chemsys import ChemicalSystem as System, from_pdb, fix_pdb
+from caddpy.chemsys import ChemicalSystem as System, from_pdb, from_npz, fix_pdb
 
 __all__ = [
     "System",
     "from_pdb",
+    "from_npz",
     "fix_pdb",
 ]
